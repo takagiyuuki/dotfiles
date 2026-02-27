64af313c824dc74f53dcab0a23d217f229070787
yuki <64290748+takagiyuuki@users.noreply.github.com>
2026-02-27 18:57:49 +0900

    Add wezterm configuration

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+return {
+  color_scheme = 'Catppuccin Mocha',
+  window_background_opacity = 0.93,
+
+  font = wezterm.font('HackGen Console NF'),
+  font_size = 12.0,
+
+  window_frame = {
+    font = wezterm.font('HackGen Console NF'),
+    font_size = 10.0,
+  }
+}
