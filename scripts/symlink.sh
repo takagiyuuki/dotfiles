49526decd68c6a7a09b2b13a2ba5155591620111
yuki <64290748+takagiyuuki@users.noreply.github.com>
2026-02-27 20:03:31 +0900

    update dotfiles

---
+++ scripts/symlink.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# make_links.sh
+ln -sf ~/dotfiles/.config/zsh/.zshrc ~/.zshrc
+ln -sfn ~/dotfiles/.config/nvim ~/.config/nvim
+ln -sfn ~/dotfiles/wezterm/ ~/.config/wezterm
+
---
+++ scripts/symlink_windows.ps1
@@ -0,0 +1,3 @@
+## Userは要変更
+New-Item -ItemType SymbolicLink -Path "$HOME\.config\wezterm" -Target "\\wsl.localhost\Ubuntu\home\user\dotfiles\.config\wezterm"
+
