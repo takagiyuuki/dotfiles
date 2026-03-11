1abee85ccd0f2e53eedaf33328569f2f9f36ab27
yuki <64290748+takagiyuuki@users.noreply.github.com>
2026-03-11 17:31:04 +0900

    Add wezterm configuration

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,15 @@
 local wezterm = require 'wezterm' --@type wezterm
 local config = wezterm.config_builder() --@type config
 
+-- keybind settings
+-- config.keys = require("keybinds").keys
+-- config.key_tables = require("keybinds").key_tables
+-- config.disable_default_key_bindings = true
+
 -- Windows settings
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   -- default current directory
-  config.default_cwd = "C:\\"
+  config.default_cwd = "C:/"
   -- default shell
   config.default_prog = {'wsl.exe', '-d', 'Ubuntu' }
   -- launcher menu
@@ -26,19 +31,26 @@ end
 
 -- Linux settings
 
--- Appearance settings
-config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 0.93
+-- Basic settings
+config.enable_scroll_bar = true
+config.exit_behavior="Hold"
 
 -- Font configuration
 local main_font = 'HackGen Console NF'
 config.font = wezterm.font(main_font)
 config.font_size = 12.0
 
+-- Appearance settings
+config.color_scheme = 'Catppuccin Mocha'
+config.window_background_opacity = 0.93
 config.window_frame = {
   font = wezterm.font(main_font),
   font_size = 10.0,
 }
+
+-- Tabs
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
 
--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -10,7 +10,7 @@ fi
 
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
+export PATH="$HOME/.luarocks/bin:$PATH:/opt/nvim-linux-x86_64/bin"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time Oh My Zsh is loaded, in which case,
